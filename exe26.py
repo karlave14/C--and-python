d8630e1f15c08a1d8ab20e66b06cdf9b20100e59
karlave14 <karla_valdeze14@hotmail.com>
2020-07-12 14:41:19 -0500

    completo

--- exe26.py
+++ exe26.py
@@ -1,26 +1,26 @@
 print("How old are you?", end=' ') #Impresión de salida
 age = input() #entra la variable
-print("How tall are you?", end=' ') #solo salida 
-tall = input() # se agrego la entrada de la variable tall 
-print("How much do you weigh?", end=' ') # faltaba un parentesis 
-weight = input() #entrada de la variable 
+print("How tall are you?", end=' ') #solo salida
+tall = input() # se agrego la entrada de la variable tall
+print("How much do you weigh?", end=' ') # faltaba un parentesis
+weight = input() #entrada de la variable
 
-print(f"So, you're {age} old, {tall} tall and {weight} heavy.") #cambiar el nombre 
+print(f"So, you're {age} old, {tall} tall and {weight} heavy.") #cambiar el nombre
 
-from sys import argv # importar biblioca para los comandos de argv y menejar el script  
-script, filename = "argv"
-
+from sys import argv # importar biblioca para los comandos de argv y menejar el script
+script, filename = argv
+print(filename)
 txt = open(filename) #corregir la palabra a filename
 
-print("Here's your file {filename}:")
-print(txt.read()) #escribir bien el txt 
+print(f"Here's your file {filename}:") # Agregamos la f para formatear la cadena de texto
+print(txt.read()) #escribir bien el txt
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read()) #se remplazo un guion por un punto .
 
 
 print("Let's practice everything.") # debe ser con comillas dobles
@@ -41,13 +41,13 @@ print(poem)
 print("--------------") #abrir comillas
 
 
-five = 10 - 2 + 3 - 6 #agregar el 6 
+five = 10 - 2 + 3 - 6 #agregar el 6
 print(f"This should be five: {five}") #cerrar print
 
 def secret_formula(started): #añadir :
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars / 100 #dividirlo 
+    crates = jars / 100 #dividirlo
     return jelly_beans, jars, crates
 
 
@@ -82,7 +82,7 @@ if people < cats:
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs: #cerrar con : 
+if people > dogs: #cerrar con :
     print("The world is dry!")
 
 
@@ -91,10 +91,10 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs: #cerrar con : 
+if people <= dogs: #cerrar con :
     print("People are less than or equal to dogs.") #cerrar comillas dobles ""
 
 
-if people == dogs: #agregar == boleean 
+if people == dogs: #agregar == boleean
     print("People are dogs.")
 
